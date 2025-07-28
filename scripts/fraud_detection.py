c46b21d5687a69a203ba51cd2553847a30e2aa5f
Kavita539 <pathakkavita273@gmail.com>
2025-07-28 18:54:54 +0530

    Created and visualized the dataset

---
+++ scripts/fraud_detection.py
@@ -0,0 +1,60 @@
+import random
+import faker
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+from faker import Faker
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+num_of_data = 50000
+fraud_ratio = 0.01
+large_data = []
+faker = Faker() ## creating an instance for not getting error
+
+def random_date(start : datetime, end : datetime) -> datetime: ##creating a function for using in timestamp
+    delta = end - start
+    int_delta = delta.days * 24 * 60 * 60
+    random_second = random.randint(0, int_delta)
+    return start + timedelta(seconds=random_second)
+
+# start and end date created for timestamp
+start_date = datetime(2023, 1, 1)
+end_date = datetime(2024, 12, 31)
+merchants = ["Amazon", "Walmart", "Target", "Uber", "Starbucks", "Apple", "Best Buy", "Netflix", "Costco", "Nike"]
+locations = [ "New York", "Los Angeles", "Chicago", "Houston", "Phoenix","San Francisco", "Seattle", "Atlanta", "Boston", "Miami"]
+devices = ["Mobile", "Desktop", "Tablet", "POS Terminal", "Smartwatch"]
+
+
+# list of all the sequences 
+
+for i in range(num_of_data):
+    is_fraud = 1 if random.random() < fraud_ratio else 0
+    amount = round(np.random.exponential(scale=100),2)
+    if is_fraud:
+        amount *= random.randint(5,20)
+    row = {
+        "transactionID" : i+1,
+        "timestamp" : random_date(start_date,end_date),
+        "amount" : amount,
+        "merchants" : random.choice(merchants),
+        "locations" : random.choice(locations),
+        "devices" : random.choice(devices),
+        "card-holder" : faker.name(),
+        "class" : is_fraud
+    }
+    # print(amount)
+    # print(is_fraud)
+    large_data.append(row)
+
+df_large_realistic = pd.DataFrame(large_data) ## converting data into dataframe
+df_large_realistic.to_csv("Synthetic-Fraud-Detection-Dataset" , index=False) ## converting into csv file
+print("Dataset generated")
+print(df_large_realistic["class"].value_counts())
+print(df_large_realistic.head())
+print("DATASET OVERVIEW: ")
+print(f"dataset shape : {df_large_realistic.shape}")
+print(f"dataset colums: {df_large_realistic.columns.to_list()}")
+print(f"Dataset class distributions: ")
+print(f"Fraud detection percentage: {df_large_realistic["class"].mean()*100:.4f}")
+#  with this we have created and overviewd our dataset
